10fba90b8ad0fa305e946b5bf3858b9abb179dd0
cedricthecoder <nnngaboced@gmail.com>
2023-10-08 00:34:59 +0300

    IQ

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
-  * main - this is the main function
-  *
-  * Return: always 0
-  */
+ * main - this is the main function
+ *
+ * Return: 0 when successful
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
