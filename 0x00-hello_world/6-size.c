64548c9b597dd8964c395e831e126bc64c3023d0
cedricthecoder <nnngaboced@gmail.com>
2023-10-08 01:31:37 +0300

    Pretty woman.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n
-		Size of an int: 4 byte(s)\n
-		Size of a long int: 4 byte(s)\n
-		Size of a long long int: 8 byte(s)\n
-		Size of a float: 4 byte(s)");
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
 }
