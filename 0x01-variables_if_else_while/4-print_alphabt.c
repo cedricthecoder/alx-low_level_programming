c17dba2604e19aaec5e08bd428a00b2a966508fb
cedricthecoder <nnngaboced@gmail.com>
2023-10-09 14:26:47 +0300

    Howdy

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+		if (i != 'q' && i != 'e')
+			putchar(l);
+	putchar("\n");
+	return (0);
+}
