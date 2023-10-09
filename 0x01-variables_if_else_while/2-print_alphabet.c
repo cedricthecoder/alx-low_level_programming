4840417ef3389afd89fa6d34f89cc883a5437528
cedricthecoder <nnngaboced@gmail.com>
2023-10-09 12:14:13 +0300

    Apple Pie

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - This is the main function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{ putchar(letter);
+	 letter++;
+	}
+	putchar('\n');
+	return 0;
+}
