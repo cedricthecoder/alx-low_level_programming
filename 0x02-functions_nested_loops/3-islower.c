ebddb006dbba8025aff38e79418bee459ecb1276
cedricthecoder <nnngaboced@gmail.com>
2023-10-11 21:44:30 +0300

    Object Status

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _islower - checks for lowercase character
+ * @c: parameter to be checked
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c<= 'z')
+		return (1);
+	else
+		return (0);
+}
