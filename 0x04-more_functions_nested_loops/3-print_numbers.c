beeae0d55c7a8adba1fca2cfbaeda17521b3e22f
cedricthecoder <nnngaboced@gmail.com>
2023-10-13 04:24:02 +0300

    Multitask

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *print_numbers - prints numbers followed by new line
 *Return: void
@@ -8,6 +9,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+		putchar(i + '0');
+	putchar('\n');
 }
