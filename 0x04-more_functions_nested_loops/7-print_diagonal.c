091d2193a3a7d60411df5e5cc12edc98b212f943
cedricthecoder <nnngaboced@gmail.com>
2023-10-14 00:42:46 +0300

    Gotta Have it

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,22 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
+	{
 		putchar('\n');
+	}
 	else
 	{
 		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
-				putchar(' ');
-			putchar('\\');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
 			putchar('\n');
 		}
 	}
