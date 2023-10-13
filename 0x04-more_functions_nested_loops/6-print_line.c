c15db8cf50be6612064924d46d280c1b6c3b4d88
cedricthecoder <nnngaboced@gmail.com>
2023-10-13 05:36:36 +0300

    Win Win Win

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_line - sum formula
+*@n: parameters
+*Return: void
+*/
+void print_line(int n)
+{
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			putchar('_');
+		putchar('\n');
+	}
+}
