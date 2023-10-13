1dd44c2e8132f17e2d25407be1fc3de1da567796
cedricthecoder <nnngaboced@gmail.com>
2023-10-13 05:26:05 +0300

    J Station

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			putchar(j + '0');
+			if (j > 9)
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 		}
 		putchar('\n');
 	}
-	putchar('\n');
 }
