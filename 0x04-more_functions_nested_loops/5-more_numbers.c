97a2e4611e37b55548aeced9a26e2caac011d9a7
cedricthecoder <nnngaboced@gmail.com>
2023-10-13 05:05:48 +0300

    Unlucky

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*void more_numbers - Some random function
+*more_numbers - Some random function
 *Return: 0
 */
 void more_numbers(void)
@@ -10,7 +10,7 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 14; i++)
+		for (j = 0; j <= 14; i++)
 			putchar(j + '0');
 		putchar('\n');
 	}
