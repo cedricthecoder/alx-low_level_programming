60cc2839a54b3f664948fb78edec1950b83c301e
cedricthecoder <nnngaboced@gmail.com>
2023-10-13 04:38:50 +0300

    Give away

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
 */
 void print_most_numbers(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		if i != 2 && i != 4
+		if (i != 2 && i != 4)
 			putchar(i + '0');
 	}
 	putchar('\n');
