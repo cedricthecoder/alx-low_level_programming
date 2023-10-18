59fd571fbd64098e8feefd51d7eef988a7d9d59a
cedricthecoder <nnngaboced@gmail.com>
2023-10-18 15:36:23 +0300

    Some random Meltdown

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strcmp - probably bad
+*@s1: parameter 1
+*@s2: parameter 2
+*Return: Void
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
