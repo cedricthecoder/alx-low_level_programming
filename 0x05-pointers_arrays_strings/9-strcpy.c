9ea6d84326cb652171bb28edf5dcf94661118a0f
cedricthecoder <nnngaboced@gmail.com>
2023-10-18 04:14:25 +0300

    Prank

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* char *_strcpy - some function
+* @dest: some parameter
+* @src: some other parameter
+* return: string
+*/
+char *_strcopy(char *dest, char *src)
+{
+	int l = 0;
+	int x;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (x = 0; x < 1; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
