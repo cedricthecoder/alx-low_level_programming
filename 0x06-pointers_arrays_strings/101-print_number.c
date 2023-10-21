d4206ad63592144526e9e5193874aeee0dcd1499
cedricthecoder <nnngaboced@gmail.com>
2023-10-21 17:46:43 +0300

    Organize

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,9 +11,16 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
+	if (n == 0)
 	{
-		print_number(n / 10);
+		_putchar('0');
 	}
-	_putchar((n % 10) + '0');
+	else
+	{
+		if (n / 10 != 0)
+		{
+			print_number(n / 10);
+		}
+		_putchar((n % 10) + '0');
+	}
 }
