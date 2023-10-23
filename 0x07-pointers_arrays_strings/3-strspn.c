7dfaa548fa3e985cd7a6f31d23755f0731d9a40c
cedricthecoder <nnngaboced@gmail.com>
2023-10-23 22:29:24 +0300

    Squid Game

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strspn - Gets the length of a prefix substring.
+* @s: The string to search.
+* @accept: The string containing accepted characters.
+* Return: The number of bytes in initial segment has stuff from accept.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int found;
+
+	while (*s)
+	{
+		found = 0;
+		for (int i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			return (count);
+		}
+		count++;
+		s++;
+	}
+	return (count);
+}
