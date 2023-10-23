ea1ddb762b3dc3ae334ba2a8a50289bdc79bbca9
cedricthecoder <nnngaboced@gmail.com>
2023-10-24 00:46:28 +0300

    Other meaningful commit message.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,6 +17,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - 1 - i)];
 	}
-	printf("Sum of main diagonal: %d\n", sum1);
-	printf("Sum of other diagonal: %d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);}
 }
