3cc604e53cd79f3fb0404d53ef313a8b0be16609
cedricthecoder <nnngaboced@gmail.com>
2023-11-02 01:03:25 +0200

    What If?

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *main - this is the main function
+  *_puts - some other function
+  *Return: 0
+  */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
