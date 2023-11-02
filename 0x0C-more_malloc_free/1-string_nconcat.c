51554516de1c52f1e2928c732181c136a2acf4d7
cedricthecoder <nnngaboced@gmail.com>
2023-11-02 15:48:03 +0200

    Had I known

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,23 +10,23 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	char *conc;
+	unsigned int len = n, ind;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (index = 0; s1[index]; index++)
+	for (ind = 0; s1[ind]; ind++)
 		len++;
-	concat = malloc(sizeof(char) * (len + 1));
-	if (concat == NULL)
+	conc = malloc(sizeof(char) * (len + 1));
+	if (conc == NULL)
 		return (NULL);
 	len = 0;
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-	concat[len] = '\0';
-	return (concat);
+	for (ind = 0; s1[ind]; ind++)
+		conc[len++] = s1[ind];
+	for (ind = 0; s2[ind] && ind < n; ind++)
+		conc[len++] = s2[ind];
+	conc[len] = '\0';
+	return (conc);
 }
