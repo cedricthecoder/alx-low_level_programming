04aa3b0278d5206b8c3bf0093a81d3de715b9378
cedricthecoder <nnngaboced@gmail.com>
2023-11-06 23:14:11 +0200

    I thought you wanted to dance

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+*struct dog - a dog
+*@name: name
+*@age: age
+*@owner: owner
+*Description: A dog
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
