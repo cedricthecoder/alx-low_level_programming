7851b7aa2d5c034a67ae53dce95a04e1bba6994e
cedricthecoder <nnngaboced@gmail.com>
2023-11-09 00:47:28 +0200

    Very random day

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,9 +8,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	unsigned int j;
 
 	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+		for (j = 0; j < size; j++)
+			action(array[j]);
 }
