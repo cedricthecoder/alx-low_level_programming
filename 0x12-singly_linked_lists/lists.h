5b4e7f31d722345b6a85b73777af19bcb4371230
cedricthecoder <nnngaboced@gmail.com>
2023-11-16 23:08:42 +0200

    list commit file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,26 +2,26 @@
 #define LISTS_H
 
 /*
-* Desc: header file containinf prototypes and definitions
-* for all functions and types written in this proj
-*/
+   * Desc: header file containinf prototypes and definitions
+   * for all functions and types written in this proj
+   */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- * Description: singly linked list node structure
- *              for Holberton project
- */
+   * struct list_s - singly linked list
+    * @str: string - (malloc'ed string)
+     * @len: length of the string
+      * @next: points to the next node
+       *
+        * Description: singly linked list node structure
+	 *              for Holberton project
+	  */
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
